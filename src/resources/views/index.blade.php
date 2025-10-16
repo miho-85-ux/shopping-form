0d1bc2736f764a5fd4869ff61b2da46082402043
yokoyama miho <mmmiho0805mm@hotmail.co.jp>
2025-10-16 09:16:00 +0900

    index.blade編集中

---
+++ src/resources/views/index.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.app')
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/index.css') }}">
+@endsection
+
+@section('content')
+    <div class="shopping-content">
+        <div class="shopping-content__title">
+            <h2 class="create-title">買うもの</h2>
+            <form class="create-form">
+                    <input class="create-text" type="text"  name="text">
+            </form>
+        </div>
+        <div class="shopping-content__title">
+            <h2 class="create-title">個数</h2>
+            <form class="create-form__select-box">
+                <select name="quantity" id="quantity">
+                    @foreach (range(1, 10) as $num)
+                    <option value="{{ $num }}">{{ $num }}個</option>
+                    @endforeach
+                </select>
+            </form>
+        </div>
+        <div class="create-form__button">
+            <button class="create-form__button-submit"  type="submit">登録</button>
+        </div>
+    </div>
+
+    <div class="shopping-table">
+        <table class="shopping-table__inner">
+            <tr class="shopping-table__row">
+                <th class="shopping-table__header">
+                    <span class="shopping-table__header-span">買い物リスト</span>
+                    <span class="shopping-table__header-span">個数</span>
+                    <span class="shoppind-table__header__span">金額</span>
+                </th>
+            </tr>
+            <tr class="shopping-table__row">
+
+        </table>
+    </div>
+
+@endsection
